528d2e21390a4502336c8b61d962d84dfa931e49
YeonJangL <lej32552002@gmail.com>
2024-03-04 11:14:27 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random rand = new Random();
+        int[] arr = new int[6];
+
+        int index = 0;
+
+        while (index < 6)
+        {
+            int r = rand.Next(1, 46);
+
+            if (!Is_arr(arr, r))
+            {
+                arr[index] = r;
+                index++;
+                Console.WriteLine(r);
+            }
+        }
+    }
+
+    static bool Is_arr(int[] arr, int value)
+    {
+        foreach (int item in arr)
+        {
+            if (item == value)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
